44aedc5a53c037543c42a95acae3c3682a7a56b8
Regeq <114878776+Regeq@users.noreply.github.com>
2023-12-10 20:07:41 +0100

    Update and rename about-us.html to content.js

---
+++ content.js
@@ -0,0 +1,15 @@
+// content.js
+
+const content = {
+  home: `
+    <h2>Welcome to Afton Robotics</h2>
+    <p>Explore the world of technology with us. We create innovative solutions for your needs.</p>
+    <p>Feel free to navigate through different sections to learn more about us and our projects.</p>
+  `,
+  aboutUs: `
+    <h2>About us</h2>
+    <p>We are a team made of pure love for technology. We make robots, software, and bots.</p>
+    <p>In our company, we love making everything easy and simple (not the robots). We also love when everything is clean and beautiful.</p>
+  `,
+  // Další sekce
+};
